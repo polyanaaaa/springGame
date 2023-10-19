85c41a1829632d2672d8d30d8901ecb8c7b95ba7
polyanaaaa <133990972+polyanaaaa@users.noreply.github.com>
2023-10-19 08:28:18 -0300

    Add files via upload

---
+++ PrjGame/src/main/java/com/senai/polyana/PrjGame/repositories/JogoRepository.java
@@ -0,0 +1,10 @@
+package com.senai.polyana.PrjGame.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.polyana.PrjGame.entities.*;
+
+public interface JogoRepository extends JpaRepository <jogo, Long>{
+
+
+}
---
+++ PrjGame/src/main/resources/static/main.js
@@ -0,0 +1,49 @@
+document.getElementById('cadastroForm').addEventListener('submit', cadastrarJogo);
+function cadastrarJogo(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const plataform = document.getElementById('plataform').value;
+
+    fetch('http://localhost:8080/jogos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }, body: JSON.stringify({ name, plataform }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert('Jogo cadastrado com sucesso!'); document.getElementById('cadastroForm').reset();
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar jogo:', error);
+        });
+}
+
+function pesquisarJogo() {
+    const searchId = document.getElementById('searchId').value;
+
+    fetch("http://localhost:8080/jogos/${searchId}")
+        .then(response => {
+            if (response.status === 404) {
+                return Promise.reject('Jogo não encontrado');
+            }
+            return response.json();
+        })
+
+        .then(data => {
+            const resultadoPesquisa = document.getElementById
+                ('resultadoPesquisa'); resultadoPesquisa.innerHTML = `
+    <h3>ID: ${data.id}</h3>
+    <p>Nome: ${data.name}</p>
+    <p>Plataforma: ${data.plataforma}</p>
+    `
+
+                ;
+
+        })
+        .catch(error => {
+            console.error('Erro ao pesquisar jogo:', error);
+            const resultadoPesquisa = document.getElementById('resultadoPesquisa');
+            resultadoPesquisa.innerHTML
+        });
+}
